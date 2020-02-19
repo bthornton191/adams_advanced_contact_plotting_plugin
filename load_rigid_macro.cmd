242c0d92f90de2a3b9266503165dddc01ad6a82d
Ben Thornton <ben.thornton@mscsoftware.com>
2020-02-19 14:04:22 -0500

    reorganized

--- load_rigid_macro.cmd
+++ load_rigid_macro.cmd
@@ -1,6 +1,6 @@
 macro read  &
-   macro_name = new_macro  &
-   file_name = "new_macro"  &
+   macro_name = rigid_macro  &
+   file_name = "rigid_macro"  &
    user_entered_command = "aaa"  &
    wrap_in_undo = yes  &
    create_panel = yes
